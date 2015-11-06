df8a545b6455b1819ccbf1b42340569b7c78a9da
Mattia D'Alleva <antaresone@antaresone.eu>
2015-11-07 00:19:13 +0100

    KeyEvent: disable back/menu keys wakeup
    By default AOSP has wakeup support for these two buttons which we don't need as we have a physical home button.
    
    Conflicts:
    	core/java/android/view/KeyEvent.java
    
    Change-Id: I08cd95bd734ed0f5cd42e88fe94dde48ff3aa5c0

--- core/java/android/view/KeyEvent.java
+++ core/java/android/view/KeyEvent.java
@@ -1853,8 +1853,7 @@ public class KeyEvent extends InputEvent implements Parcelable {
     /** @hide */
     public static final boolean isWakeKey(int keyCode) {
         switch (keyCode) {
-            case KeyEvent.KEYCODE_BACK:
-            case KeyEvent.KEYCODE_MENU:
+            case KeyEvent.KEYCODE_HOME:
             case KeyEvent.KEYCODE_WAKEUP:
             case KeyEvent.KEYCODE_PAIRING:
             case KeyEvent.KEYCODE_STEM_1:
