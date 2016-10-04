420ed3aae8221ba31b51fe70e36d5c17a6af228a
Varun Date <date.varun123@gmail.com>
2016-10-04 23:36:04 +0530

    Fix Derp
    Change-Id: Id665254c108ffca25cb1dd1bb1bdfc728fad7b55
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
+++ packages/SystemUI/src/com/android/systemui/recents/views/RecentsView.java
@@ -213,8 +213,14 @@ public class RecentsView extends FrameLayout {
         // Update the top level view's visibilities
         if (stack.getTaskCount() > 0) {
             hideEmptyView();
+			if (mFloatingButton != null) {
+			mFloatingButton.setVisibility(View.VISIBLE);
+			}
         } else {
             showEmptyView(R.string.recents_empty_message);
+			if (mFloatingButton != null) {
+			mFloatingButton.setVisibility(View.GONE);
+			}
         }
     }
 
@@ -296,12 +302,6 @@ public class RecentsView extends FrameLayout {
      */
     public void showEmptyView(int msgResId) {
         mTaskStackView.setVisibility(View.INVISIBLE);
-		if (mFloatingButton != null) {
-		mFloatingButton.setVisibility(View.GONE);
-		}
-	    if (mClearRecents != null) {
-		mClearRecents.setVisibility(View.GONE);
-		}
         mEmptyView.setText(msgResId);
         mEmptyView.setVisibility(View.VISIBLE);
         mEmptyView.bringToFront();
