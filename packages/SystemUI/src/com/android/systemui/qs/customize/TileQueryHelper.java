8f5d901521f7dbb64fed4d3e5640b005a805ac3d
Alex Cruz <mazdarider23@gmail.com>
2017-01-24 11:14:46 +0100

    Expose external qs tile tint color
    This should allow themers to get around issues like this (see pic below)
    
    https://i.imgur.com/cG2OzRT.jpg
    
    Change-Id: If6dbf9ab29f8007d85a3c45524b1cf4ba1b032fb

--- packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
+++ packages/SystemUI/src/com/android/systemui/qs/customize/TileQueryHelper.java
@@ -180,8 +180,6 @@ public class TileQueryHelper {
                 }
                 icon.mutate();
                 icon.setTint(mContext.getColor(R.color.external_qs_tile_tint_color));
-                icon.setTintMode(Mode.SRC_ATOP);
-                CharSequence label = info.serviceInfo.loadLabel(pm);
                 addTile(spec, icon, label != null ? label.toString() : "null", appLabel, mContext);
             }
             return tiles;
