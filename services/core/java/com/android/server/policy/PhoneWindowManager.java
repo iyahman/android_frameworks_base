98e537bdf313f1ed143ffb9944dcc5722bc2e944
dkati <dkatikaridis@gmail.com>
2017-01-29 01:29:53 +0200

    core:Fix device behavor when we use lid cover case
    Ported to Nougat with current LineageOS edits
    Originally posted:https://github.com/JDCTeam/android_frameworks_base/commit/b5bceda19e75a3725a26e131cc1a824e44a26c04
    
    Change-Id: I9d5931b3e3953417e2247a0cc0868fd0b043c6ac

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -8067,13 +8067,28 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private void applyLidSwitchState() {
         mPowerManager.setKeyboardVisibility(isBuiltInKeyboardVisible());
 
-        if (mLidState == LID_CLOSED && mLidControlsSleep) {
-            mPowerManager.goToSleep(SystemClock.uptimeMillis(),
-                    PowerManager.GO_TO_SLEEP_REASON_LID_SWITCH,
-                    PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE);
-        } else if (mLidState == LID_CLOSED && mLidControlsScreenLock) {
-            mWindowManagerFuncs.lockDeviceNow();
-        }
+	if (mLidControlsSleep) {
+		IDreamManager dreamManager = getDreamManager();
+		if ( dreamManager != null ) {
+			try {
+				dreamManager.setLidState(mLidState);
+			}catch ( RemoteException e) {
+			}
+		}
+		if (mLidState == LID_CLOSED) {
+		if (mFocusedWindow != null && (mFocusedWindow.getAttrs().flags
+				& WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON) != 0) {
+		     // if an application requests that the screen be turned on
+		     // and there's a closed device cover, don't turn the screen off!
+		     return ;
+		}
+		mPowerManager.goToSleep(SystemClock.uptimeMillis(),
+			PowerManager.GO_TO_SLEEP_REASON_LID_SWITCH,
+			PowerManager.GO_TO_SLEEP_FLAG_NO_DOZE);
+		} else if (mLidState == LID_CLOSED && mLidControlsScreenLock) {
+		mWindowManagerFuncs.lockDeviceNow();
+		}
+	}
 
         synchronized (mLock) {
             updateWakeGestureListenerLp();
